3a3de60e498ea01a6a77f85051d481888b7c16e6
joscarrr <emilianormzdiaz@gmail.com>
2026-09-23 17:57:31 -0600

    PER-2: Format i18n setup and messages with Prettier

--- i18n/request.ts
+++ i18n/request.ts
@@ -3,12 +3,12 @@ import { getRequestConfig } from "next-intl/server";
 import { routing } from "./routing";
 
 export default getRequestConfig(async ({ locale: requested }) => {
-  const locale = hasLocale(routing.locales, requested)
-    ? requested
-    : routing.defaultLocale;
+	const locale = hasLocale(routing.locales, requested)
+		? requested
+		: routing.defaultLocale;
 
-  return {
-    locale,
-    messages: (await import(`../app/messages/${locale}.json`)).default,
-  };
+	return {
+		locale,
+		messages: (await import(`../app/messages/${locale}.json`)).default,
+	};
 });
