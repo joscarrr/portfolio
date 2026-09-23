5b7b3acb0112fe088bc944e9d208fa40c0fc41d7
joscarrr <emilianormzdiaz@gmail.com>
2026-09-23 17:57:31 -0600

    PER-2: Format config files with Prettier

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ import createMDX from "@next/mdx";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  pageExtensions: ["ts", "tsx", "md", "mdx"],
+	pageExtensions: ["ts", "tsx", "md", "mdx"],
 };
 
 const withMDX = createMDX({});
